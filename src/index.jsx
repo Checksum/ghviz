35108b33e24f436c475e1fdea30ee0ecc000dfe0
Srinath Sankar <srinath@iambot.net>
2019-05-07 20:05:54 +1000

    remove initial loading indicator

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,6 @@ import ReactDOM from "react-dom";
 import "normalize.css";
 import "./style.css";
 
-ReactDOM.render(<p>Loading...</p>, document.querySelector("#root"));
-
 import("./App").then(AppModule => {
   const App = AppModule.default;
   ReactDOM.render(<App />, document.querySelector("#root"));
