96c2ba1ac8a76861bf0926d41ea63417ec5588be
Srinath Sankar <srinath@iambot.net>
2019-05-11 12:20:12 +1000

    initial loader

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,6 @@ import "../lib/vendor";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "normalize.css";
-import "./style.css";
-
 import("./App").then(AppModule => {
   const App = AppModule.default;
   ReactDOM.render(<App />, document.querySelector("#root"));
