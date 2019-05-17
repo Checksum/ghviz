ea7951dcbf9241bd4c39dc4ef58cf502d7c8895c
Srinath Sankar <srinath@iambot.net>
2019-05-17 13:32:10 +1000

    viz spinner
    [skip ci]

--- src/viz/Visualization.js
+++ src/viz/Visualization.js
@@ -83,16 +83,16 @@ export default function visualization(WrappedComponent) {
               alignItems="center"
               justifyContent="center"
               style={{
-                position: "fixed",
+                position: "absolute",
                 top: 0,
                 bottom: 0,
                 left: 0,
                 right: 0,
                 background: "rgba(0, 0, 0, 0.15)",
-                zIndex: 100
+                zIndex: 10
               }}
             >
-              <div className="spinner">
+              <div className="spinner" style={{ top: "-10%" }}>
                 <div className="double-bounce1" />
                 <div className="double-bounce2" />
               </div>
